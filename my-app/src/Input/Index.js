0ce4ee4599b4072aa7f0e1f62d354315cd2558e1
trisrosco <trisrosco@gmail.com>
2023-04-14 13:26:37 +0100

    cleaned up redundant code

--- my-app/src/Input/Index.js
+++ my-app/src/Input/Index.js
@@ -7,17 +7,6 @@ function Input(props) {
   const [inputValue, setInputValue] = useState("");
   const [items, setItems] = useState([]);
 
-  const handleButtonClick = () => {
-    const newItem = { id: Date.now(), value: inputValue };
-    setItems([...items, newItem]);
-    setInputValue("");
-  };
-
-  const handleDeleteItem = (id) => {
-    const newItems = items.filter((item) => item.id !== id);
-    setItems(newItems);
-  };
-
   return (
     <div className="Input">
       <input
@@ -25,15 +14,6 @@ function Input(props) {
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)}
       />
-      <button onClick={handleButtonClick}>Add button</button>
-      <ul>
-        {items.map((item) => (
-          <li key={item.id}>
-            {item.value}
-            <button onClick={() => handleDeleteItem(item.id)}>Delete</button>
-          </li>
-        ))}
-      </ul>
     </div>
   );
 }
